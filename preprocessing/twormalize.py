f831ff28361861a94691e8976d6afa46d247d26f
tttthomasssss <th0mas.ko6er@gmail.com>
2014-12-29 19:36:12 +0000

    more tokenisation

---
+++ preprocessing/twormalize.py
@@ -0,0 +1,14 @@
+__author__ = 'thomas'
+import twokenize
+
+
+def normalise_urls(tokens): # Twitter URLs should all be of the form http://t.co/<BLAHBLAH>, so this should be safe
+	return [t if not t.startswith('http:') else 'HTTPLINK' for t in tokens]
+
+
+def normalise_numbers(tokens): # Only actual numbers such as 3.223 or 59 at the moment
+	return [t if not (isinstance(t, float) or isinstance(t, int)) else 'NUM' for t in tokens]
+
+
+def tokenise_and_normalise(text):
+	return normalise_numbers(normalise_urls(twokenize.tokenize(text)))
