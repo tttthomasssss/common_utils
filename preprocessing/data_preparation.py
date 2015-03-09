30fdd69dca0b5b65bc03e51b1d4acc4bf84cfbff
tttthomasssss <tttthomasssss@gmail.com>
2015-03-09 14:39:34 +0000

    fallback to shufflesplit in case of fail

--- preprocessing/data_preparation.py
+++ preprocessing/data_preparation.py
@@ -1,5 +1,6 @@
 __author__ = 'thomas'
 
+from sklearn.cross_validation import ShuffleSplit
 from sklearn.cross_validation import StratifiedShuffleSplit
 
 
@@ -9,7 +10,11 @@ def split_data_raw(dataset, train_data_idx, train_labels_idx, test_data_idx, tes
 
 	if (test_data_idx == -1 and unlabelled_data_idx == -1): # Going to be the RCV1 Dataset, plenty of data, so the splits should be reasonably sized
 		# Do a 0.8 (train & unlabelled) vs. 0.2 (test) split
-		split = StratifiedShuffleSplit(y_train, n_iter=1, test_size=0.2, random_state=random_state)
+		try:
+			split = StratifiedShuffleSplit(y_train, n_iter=1, test_size=0.2, random_state=random_state)
+		except ValueError: # happens if not enough labels are available for a stratified split
+			split = ShuffleSplit(y_train.shape[0], n_iter=1, test_size=0.2, random_state=random_state)
+
 		for train_idx, test_idx in split:
 			# Loop over the stuff, because lists don't do fancy indexing
 			X_train_raw = []
@@ -22,7 +27,11 @@ def split_data_raw(dataset, train_data_idx, train_labels_idx, test_data_idx, tes
 			y_train, y_test = y_train[train_idx], y_train[test_idx]
 
 		# Do another 0.8 (unlabelled) vs. 0.2 (train) split
-		split = StratifiedShuffleSplit(y_train, n_iter=1, test_size=0.2, random_state=random_state)
+		try:
+			split = StratifiedShuffleSplit(y_train, n_iter=1, test_size=0.2, random_state=random_state)
+		except ValueError: # happens if not enough labels are available for a stratified split
+			split = ShuffleSplit(y_train.shape[0], n_iter=1, test_size=0.2, random_state=random_state)
+
 		for unlabelled_idx, train_idx in split:
 			X_train_raw = []
 			for idx in train_idx:
@@ -35,7 +44,11 @@ def split_data_raw(dataset, train_data_idx, train_labels_idx, test_data_idx, tes
 
 	elif (test_data_idx == -1 and unlabelled_data_idx != -1): # Twitter Datasets, not much labelled data
 		# Do a 0.7 (train) vs. 0.3 (test)
-		split = StratifiedShuffleSplit(y_train, n_iter=1, test_size=0.3, random_state=random_state)
+		try:
+			split = StratifiedShuffleSplit(y_train, n_iter=1, test_size=0.3, random_state=random_state)
+		except ValueError: # happens if not enough labels are available for a stratified split
+			split = ShuffleSplit(y_train.shape[0], n_iter=1, test_size=0.3, random_state=random_state)
+
 		for train_idx, test_idx in split:
 			X_train_raw = []
 			for idx in train_idx:
@@ -50,7 +63,11 @@ def split_data_raw(dataset, train_data_idx, train_labels_idx, test_data_idx, tes
 
 	elif (test_data_idx != -1 and unlabelled_data_idx == -1): # ApteMod & 20Newsgroups, splits should be appropriately sized
 		# Do a 0.7 (unlabelled) vs. 0.3 (train)
-		split = StratifiedShuffleSplit(y_train, n_iter=1, test_size=0.3, random_state=random_state)
+		try:
+			split = StratifiedShuffleSplit(y_train, n_iter=1, test_size=0.3, random_state=random_state)
+		except ValueError: # happens if not enough labels are available for a stratified split
+			split = ShuffleSplit(y_train.shape[0], n_iter=1, test_size=0.3, random_state=random_state)
+
 		for unlabelled_idx, train_idx in split:
 			X_train_raw = []
 			for idx in train_idx:
@@ -77,20 +94,32 @@ def split_data(dataset, train_data_idx, train_labels_idx, test_data_idx, test_la
 
 	if (test_data_idx == -1 and unlabelled_data_idx == -1): # Going to be the RCV1 Dataset, plenty of data, so the splits should be reasonably sized
 		# Do a 0.8 (train & unlabelled) vs. 0.2 (test) split
-		split = StratifiedShuffleSplit(y_train, n_iter=1, test_size=0.2, random_state=random_state)
+		try:
+			split = StratifiedShuffleSplit(y_train, n_iter=1, test_size=0.2, random_state=random_state)
+		except ValueError: # happens if not enough labels are available for a stratified split
+			split = ShuffleSplit(y_train.shape[0], n_iter=1, test_size=0.2, random_state=random_state)
+
 		for train_idx, test_idx in split:
 			X_train, X_test = X_train[train_idx], X_train[test_idx]
 			y_train, y_test = y_train[train_idx], y_train[test_idx]
 
 		# Do another 0.8 (unlabelled) vs. 0.2 (train) split
-		split = StratifiedShuffleSplit(y_train, n_iter=1, test_size=0.2, random_state=random_state)
+		try:
+			split = StratifiedShuffleSplit(y_train, n_iter=1, test_size=0.2, random_state=random_state)
+		except ValueError: # happens if not enough labels are available for a stratified split
+			split = ShuffleSplit(y_train.shape[0], n_iter=1, test_size=0.2, random_state=random_state)
+
 		for unlabelled_idx, train_idx in split:
 			X_unlabelled, X_train = X_train[unlabelled_idx], X_train[train_idx]
 			y_train = y_train[train_idx]
 
 	elif (test_data_idx == -1 and unlabelled_data_idx != -1): # Twitter Datasets, not much labelled data
 		# Do a 0.7 (train) vs. 0.3 (test)
-		split = StratifiedShuffleSplit(y_train, n_iter=1, test_size=0.3, random_state=random_state)
+		try:
+			split = StratifiedShuffleSplit(y_train, n_iter=1, test_size=0.3, random_state=random_state)
+		except ValueError: # happens if not enough labels are available for a stratified split
+			split = ShuffleSplit(y_train.shape[0], n_iter=1, test_size=0.3, random_state=random_state)
+
 		for train_idx, test_idx in split:
 			X_train, X_test = X_train[train_idx], X_train[test_idx]
 			y_train, y_test = y_train[train_idx], y_train[test_idx]
@@ -99,7 +128,11 @@ def split_data(dataset, train_data_idx, train_labels_idx, test_data_idx, test_la
 
 	elif (test_data_idx != -1 and unlabelled_data_idx == -1): # ApteMod & 20Newsgroups, splits should be appropriately sized
 		# Do a 0.7 (unlabelled) vs. 0.3 (train)
-		split = StratifiedShuffleSplit(y_train, n_iter=1, test_size=0.3, random_state=random_state)
+		try:
+			split = StratifiedShuffleSplit(y_train, n_iter=1, test_size=0.3, random_state=random_state)
+		except ValueError: # happens if not enough labels are available for a stratified split
+			split = ShuffleSplit(y_train.shape[0], n_iter=1, test_size=0.3, random_state=random_state)
+
 		for unlabelled_idx, train_idx in split:
 			X_unlabelled, X_train = X_train[unlabelled_idx], X_train[train_idx]
 			y_train = y_train[train_idx]
