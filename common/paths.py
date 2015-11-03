f658c372756f538455947258354a5ac11b69e66c
tttthomasssss <th0mas.ko6er@gmail.com>
2015-11-03 10:06:46 +0000

    added log path to paths

--- common/paths.py
+++ common/paths.py
@@ -4,32 +4,42 @@ import socket
 
 PROJECT_PATH = os.path.dirname(os.path.dirname(__file__))
 
-__BASEPATH__ = {
+_BASEPATH = {
 	'Thomas-Kobers-MacBook.local': os.path.expanduser('~/DevSandbox/InfiniteSandbox/tag-lab'),
 	'm011437.inf.susx.ac.uk': os.path.expanduser('~/DevSandbox/InfiniteSandbox/tag-lab'),
 	'disco': os.path.expanduser('~')
 }
 
-__DATASET_PATH__ = {
+_DATASET_PATH = {
 	'Thomas-Kobers-MacBook.local': os.path.expanduser('~/DevSandbox/EpicDataShelf/tag-lab'),
 	'm011437.inf.susx.ac.uk': os.path.expanduser('~/DevSandbox/InfiniteSandbox/_datasets/'),
 	'disco': os.path.expanduser('~/_datasets')
 }
 
-__OUT_PATH__ = {
+_OUT_PATH = {
 	'Thomas-Kobers-MacBook.local': os.path.expanduser('~/DevSandbox/InfiniteSandbox/tag-lab/_results'),
 	'm011437.inf.susx.ac.uk': os.path.expanduser('~/DevSandbox/InfiniteSandbox/_results'),
 	'disco': os.path.expanduser('~/_results')
 }
 
+_LOG_PATH = {
+	'Thomas-Kobers-MacBook.local': os.path.expanduser('~/DevSandbox/InfiniteSandbox/tag-lab/_logs'),
+	'm011437.inf.susx.ac.uk': os.path.expanduser('~/DevSandbox/InfiniteSandbox/_logs'),
+	'disco': os.path.expanduser('~/_logs')
+}
+
 
 def get_base_path():
-	return __BASEPATH__.get(socket.gethostname(), '/lustre/scratch/inf/thk22/code') # Fallback on lustre path on cluster
+	return _BASEPATH.get(socket.gethostname(), '/lustre/scratch/inf/thk22/code') # Fallback on lustre path on cluster
 
 
 def get_dataset_path():
-	return __DATASET_PATH__.get(socket.gethostname(), '/lustre/scratch/inf/thk22/_datasets') # Fallback on lustre path on cluster
+	return _DATASET_PATH.get(socket.gethostname(), '/lustre/scratch/inf/thk22/_datasets') # Fallback on lustre path on cluster
 
 
 def get_out_path():
-	return __OUT_PATH__.get(socket.gethostname(), '/lustre/scratch/inf/thk22/_results') # Fallback on lustre path on cluster
+	return _OUT_PATH.get(socket.gethostname(), '/lustre/scratch/inf/thk22/_results') # Fallback on lustre path on cluster
+
+
+def get_log_path():
+	return _LOG_PATH.get(socket.gethostname(), '/lustre/scratch/inf/thk22/_logs') # Fallback on lustre path on cluster
