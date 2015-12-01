3ac819b8751271c6187f46bc4215b4130f4edc0f
tttthomasssss <tttthomasssss@gmail.com>
2015-12-01 16:22:45 +0000

    oida bitte du oaschwarzn

--- common/dataset_utils.py
+++ common/dataset_utils.py
@@ -1433,7 +1433,7 @@ def fetch_scws_wikipedia_apt_vectors(example_id, dataset_path=os.path.join(paths
 	ext_cache_name = 'wikipedia_lc_{}{}_lemma-{}_pos-{}{}_vectors_cache.joblib'.format(dep_order, '_norm' if normalised else '', use_lemma, use_pos, '_pmi' if use_pmi else '')
 	ext_cache_path = os.path.join(paths.get_external_dataset_path(), 'word_similarity_in_ctx', 'cached_vectors', ext_cache_name)
 	if (check_ext_cache and ext_cache_path):
-		return joblib.load(os.path.join(ext_cache_path, ext_cache_name))
+		return joblib.load(ext_cache_path)
 
 	if (os.path.exists(os.path.join(dataset_path, subpath, str(example_id), fname_1)) and os.path.exists(os.path.join(dataset_path, subpath, str(example_id), fname_2))):
 		with gzip.open(os.path.join(dataset_path, subpath, str(example_id), fname_1), 'rt') as vec_cache:
